8e7378a12c5965038258cca57071f147a47b140c
Trent Moss <trentmoss05@gmail.com>
2019-02-03 00:21:32 +0000

    Automatically backed up by Learn

---
+++ lib/music_library_controller.rb
@@ -0,0 +1,79 @@
+class MusicLibraryController
+  def initialize(path = "./db/mp3s")
+    import = MusicImporter.new(path)
+    import.import
+  end
+
+  def call
+    input = ""
+    while input != "exit"
+      puts "Welcome to your music library!"
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      puts "What would you like to do?"
+      input = gets.chomp
+      case input
+      when "list songs"
+        list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
+      when "play song"
+        play_song
+      end
+    end
+  end
+
+  def list_songs
+    Song.all.each.with_index(1) do |song, i|
+      puts "#{i}. #{song.to_s}"
+    end
+  end
+
+  def list_artists
+    Artist.all.each.with_index(1) do |name, i|
+      puts "#{i}. #{name.name}"
+    end
+  end
+
+  def list_genres
+
+  end
+
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    artist_input = gets.strip
+    if artist = Artist.find_by_name(artist_input)
+      artist.songs.each.with_index(1) do |s,i|
+        puts "#{i}. #{s.name}"
+      end
+    end
+  end
+
+    def list_songs_by_genre
+      puts "Please enter the name of a genre:"
+      genre_input = gets.strip
+      if genre = Genre.find_by_name(genre_input)
+        artist.songs.each.with_index(1) do |s,i|
+          puts "#{i}. #{s.name}"
+        end
+      end
+    end
+
+    def play_song
+      puts "Which song number would you like to play?"
+      song_input = gets.strip
+      puts "Playing #{Song.all[song_input.to_i-1]}"
+    end
+
+end
